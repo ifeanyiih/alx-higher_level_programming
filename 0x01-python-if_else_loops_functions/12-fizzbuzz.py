de6a69cb449049c956ae77374d72b1e40114f456
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-11-29 14:56:19 +0000

    Add 12-fizzbuzz.py and Update README.md

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 101):
+        output = ""
+        if (i % 3 == 0):
+            output += "Fizz"
+        if (i % 5 == 0):
+            output += "Buzz"
+        if i % 3 == 0 or i % 5 == 0:
+            print(output, end=" ")
+        else:
+            print(i, end=" ")
