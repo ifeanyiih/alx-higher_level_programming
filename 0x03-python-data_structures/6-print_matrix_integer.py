b503e974aa764f5501d768448481eb87d5694e4f
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-03 15:58:03 +0000

    fix: print digit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print("{}".format(i), end=" ")
+            print("{:d}".format(i), end=" ")
         print()
