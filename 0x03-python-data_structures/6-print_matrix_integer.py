d9741f463771b4adbd89fd8b443c5d25f198950f
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-03 15:56:49 +0000

    fix: make file executable

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print("{:d}".format(i), end=" ")
+            print("{}".format(i), end=" ")
         print()
