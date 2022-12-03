24da7a63a61b2fd6b5336a14a9e637fb7b4bdeb8
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-03 16:07:09 +0000

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        if type(row) == 'list':
+        if type(row) == list:
             for i in row:
                 print("{:d}".format(i), end=" ")
         print()
-
-
-print_matrix_integer()
-print_matrix_integer([1,2,3,4])
-
