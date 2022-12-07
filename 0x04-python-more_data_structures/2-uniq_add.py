96974d7db59c4f4309991453ef0c5aeb37096c6d
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-07 18:32:56 +0000

    Add and Update file

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    my_set = set()
+    sums = 0
+
+    if my_list is None:
+        return None
+    
+    my_set = set(my_list)
+    for i in my_set:
+        sums += i
+    return sums
