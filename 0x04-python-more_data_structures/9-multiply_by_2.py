4c1217c0d8bf656ea775dd39c283e63121694972
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-08 15:49:09 +0000

    fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,8 +2,7 @@
 
 def multiply_by_2(a_dict):
     if a_dict is not None:
-        new_dict = a_dict[:]
-        for key, value in new_dict.items():
-            new_dict[key] = int(value) * 2
-        return new_dict
+        for key, value in a_dict.items():
+            a_dict[key] = value * 2
+        return a_dict
     return {}
