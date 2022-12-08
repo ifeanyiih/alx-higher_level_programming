8abcdd11ee2b05c6efef597c074638dc244c293c
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-08 01:40:43 +0000

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def best_score(a_dict):
-    maximum = 0
+    maximum = None
+    key_ = None
     if a_dict is None:
         return None
     for key, value in a_dict.items():
-        if value > maximum:
+        if type(value) is int and value > maximum:
             key_ = key
             value = value
             maximum = value
