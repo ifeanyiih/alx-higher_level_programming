97356bd3d35748dcf2b3db5b4c580534857b7761
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-21 00:44:29 +0000

    Add file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+A class Square
+
+This module creates a class Square.
+"""
+
+
+class Square:
+    """An empty Class that defines a Square
+
+    Attributes:
+        __size: private attribute
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+            size(int): size of a square
+        """
+        if not type(size) == int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves the square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        if not type(value) == int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of a square"""
+        return (self.__size ** 2)
