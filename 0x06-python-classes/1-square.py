f8cee1f1719c51c36970139e135d7fee98013e66
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-21 00:18:30 +0000

    Add file 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Create an Empty class
+
+This module creates an empty class Square.
+"""
+
+
+class Square:
+    """An empty Class that defines a Square
+
+    Attributes:
+        __size:
+    """
+    def __init__(self):
+        self.__size
