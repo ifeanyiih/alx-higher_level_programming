fd66473838a093f721f75043ab3bf5062c7abce3
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-12-23 08:53:12 +0000

    Add except clause

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,8 @@ def safe_print_list(my_list=[], x=0):
         while count < x:
             print(my_list[count], end="")
             count += 1
+    except:
+        pass
     finally:
         print()
         return count
