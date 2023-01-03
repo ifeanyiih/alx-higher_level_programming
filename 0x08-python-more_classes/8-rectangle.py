30fe9eec38accfb1ba9d17d06296edd1179d2405
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-03 01:47:42 +0000

    Add file

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+
+"""
+A class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """Defines a rectangle
+
+    Attributes:
+        __width (int): width of the rectangle
+        __height (int): height of the rectangle
+        number_of_instances (int): number of instances of the class
+        print_symbol (any): symbol to use in printing rectangle
+    """
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        """
+        Args:
+            width (int): the width
+            height (int): the height
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """returns the width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the value of the width"""
+        if type(value) not in [int]:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """returns the height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) not in [int]:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        string = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                string += str(self.print_symbol)
+            if i != (self.__height - 1):
+                string += '\n'
+        return string
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the biggest rectangle based on area"""
+        if type(rect_1) not in [Rectangle]:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) not in [Rectangle]:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() == rect_2.area():
+            return rect_1
+        if rect_1.area() > rect_2.area():
+            return rect_1
+        if rect_2.area() > rect_1.area():
+            return rect_2
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -12,3 +12,4 @@ Below is a list of files in this directory.
 - **5-rectangle.py**: A class Rectangle that ... (based on **4-rectangle.py**)
 - **6-rectangle.py**: A class Rectangle that ... (based on **5-rectangle.py**)
 - **7-rectangle.py**: A class Rectangle that ... (based on **6-rectangle.py**)
+- **8-rectangle.py**: A class Rectangle that ... (based on **7-rectangle.py**)
