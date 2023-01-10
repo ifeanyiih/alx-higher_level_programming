a61d01b1ee32e87abd29e3a69c438ac08fadcd45
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-10 22:22:04 +0000

    Add file

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Module contains a class: student
+"""
+
+
+class Student:
+    """Defines a Student
+
+    Attributes:
+        first_name (str): students first name
+        last_name (str): students last name
+        age (int): students age
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student
+
+        Args:
+            first_name (str): students first name
+            last_name (str): students last_name
+            age (int): students age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves the dictionary representation of a Student instance"""
+        desc = {}
+        if type(attrs) == list:
+            for attr in attrs:
+                if attr in self.__dict__.keys():
+                    desc[attr] = self.__dict__[attr]
+            return desc
+        return self.__dict__
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -16,3 +16,4 @@ Below is a list of files in this directory:
 - **7-add_item.p**: a script that adds all arguments to a Python list, and then save them to a file:
 - **8-class_to_json.py**: a function that returns the dictionary description with simple data structure
 - **9-student.py**: a class that defines a Student with a first_name, last_name and age
+- **10-student.py**: a class Student that defines a student by: (based on 9-student.py)
