3c770c5f8e306b75a56f003e570b0b680b6f71f3
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-10 21:00:37 +0000

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,15 @@ and then save them to a file
 """
 import json
 import sys
+from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file_name = "add_item.json"
 
-try:
-    obj = load_from_json_file(file_name)
-    for item in sys.argv[1:]:
-        obj.append(item)
-    save_to_json_file(obj, file_name)
-except FileNotFoundError:
-    save_to_json_file([], file_name)
+if not path.exists(file_name):
+    with open(file_name, 'w', encoding="utf-8") as f:
+        f.write('[]')
+obj = load_from_json_file(file_name)
+for item in sys.argv[1:]:
+    obj.append(item)
+save_to_json_file(obj, file_name)
