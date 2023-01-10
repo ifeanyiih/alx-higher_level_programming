9e0eb77788340042a3be8c1c8dcd4341280094c4
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-10 13:49:35 +0000

    fix

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,6 +6,11 @@ Module contains an empty class
 
 
 class BaseGeometry:
+    """A class with a single public method"""
 
     def area(self):
+        """
+        Raises:
+            Exception: area() is not implemented
+        """
         raise Exception("area() is not implemented")
