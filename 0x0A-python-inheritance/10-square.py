736fc2ce776fddb4a41844d7e252aca2736e282b
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-12 01:17:11 +0000

    Add file

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Module implements a class Square, which inherits
+from a class Rectangle
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+class Square(Rectangle):
+    """
+    Atrributes:
+        __size (int): integer attribute
+    """
+    def __init__(self, size):
+        """Initialization"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -18,3 +18,4 @@ Below is a list of files in this directory:
 - **test**: tests directory
 - **8-rectangle.py**: a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
 - **9-rectangle.py**:  a class Rectangle that inherits from BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py)
+- **10-square.py**: a class Square that inherits from Rectangle (9-rectangle.py):
