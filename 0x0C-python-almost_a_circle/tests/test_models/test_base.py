2d99526d1dd1792d2f90801c4fb713215fcc0156
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-16 15:59:30 +0000

    first tests for base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,8 +11,20 @@ Imports:
 import unittest
 from models.base import Base
 
+
 class BaseTestCase(unittest.TestCase):
     """Test for the class Base from Module base.py"""
-    def test_fail(self):
-        """Dummy Test"""
-        pass
+    def setUp(self):
+        """Sets up the test"""
+        self.obj = Base()
+        self.obj2 = Base(205)
+
+    def tearDown(self):
+        """Clears the test area"""
+        del self.obj
+        del self.obj2
+
+    def test_obj_id(self):
+        """Test that obj is created with an Id"""
+        self.assertEqual(self.obj.id, 1)
+        self.assertEqual(self.obj2.id, 205)
