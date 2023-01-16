92c685005516fbb0197792c802dbc3ec7fbb4164
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2023-01-16 01:36:10 +0000

    fix obj printing

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -130,7 +130,7 @@ class Rectangle(Base):
     def __str__(self):
         """String formating of class instance"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} -"\
-            f"{self.width}/{self.height}"
+            f" {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
         """Updates the Rectangle instance attributes"""
