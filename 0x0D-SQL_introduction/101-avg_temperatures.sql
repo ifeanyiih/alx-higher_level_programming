8cb87210934d97abfecbfe895c4d9868ac1ac64d
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-02-14 19:24:51 +0000

    modified file

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- Import in hbtn_0c_0 database this table dum
-USE hbtn_0c_0
-SOURCE temperatures.sql
+USE hbtn_0c_0;
+SOURCE temperatures.sql;
 
 SELECT DISTINCT city, AVG(value) AS avg_temp FROM temperatures
 GROUP BY city
