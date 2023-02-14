bb153857123a812365adc35452ebd3010ca7aec7
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-02-14 20:20:32 +0000

    fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,18 +2,15 @@
 
  ALTER DATABASE
     hbtn_0c_0
-    CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+    CHARACTER SET = utf8mb4;
 
 USE hbtn_0c_0
 ALTER TABLE
     first_table
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+    CONVERT TO CHARACTER SET utf8mb4;
 
 ALTER TABLE
     first_table
     CHANGE name name
     VARCHAR(256)
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+    CHARACTER SET utf8mb4;
