95df5dc58cf47b580ff39188d2cd92473b1044c0
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-02-15 17:03:54 +0000

    update file

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 --  a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT ts.title AS title, tsg.id AS genre_id
-	FROM tv_shows ts
-	JOIN tv_shows_genres tsg
-	ON ts.id = tsg.show_id;
+	FROM tv_shows ts, tv_shows_genres tsg
+	WHERE ts.id == tsg.show_id
+	ORDER BY title;
