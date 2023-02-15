f349fe960e7180897abc354fe108ffafdafb1257
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-02-15 19:33:46 +0000

    update

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,7 @@
 SELECT name
 FROM tv_genres tvg
 JOIN tv_show_genres tvsg
+ON tvg.id = tvsg.genre_id
 JOIN tv_shows tvs
-ON tvs.id = tvsg.show_id AND tvg.id = tvsg.genre_id
+ON tvs.id = tvsg.show_id
 WHERE tvs.title = "Dexter";
