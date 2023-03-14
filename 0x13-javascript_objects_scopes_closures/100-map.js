38b0a91cea5a4b79e2c404937023978db24ecc10
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-03-14 23:53:36 +0000

    modified

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = require('./100-data');
+const list = require('./100-data').list;
 const mapList = list.map((n, i) => n * i);
 console.log(list);
 console.log(mapList);
