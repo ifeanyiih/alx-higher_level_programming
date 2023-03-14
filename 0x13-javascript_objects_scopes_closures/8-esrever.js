7ead106bd66f4455e075e1103f3fe603d82c38ed
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-03-14 22:34:57 +0000

    modified

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
 function esrever (list) {
-	if (list === undefined) return;
-	if (list === null) return;
   const size = list.length / 2;
-  for (let i = 0, j = list.length - 1; i <= size; ++i, --j) {
+  for (let i = 0, j = list.length - 1; i < size; ++i, --j) {
     let temp = null;
     temp = list[i];
     list[i] = list[j];
