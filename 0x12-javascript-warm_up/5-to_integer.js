a94a9730f529455c2181a7461f675a4597d2e01c
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-03-14 00:19:54 +0000

    modified

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 let argv = process.argv.slice(2);
-let num = Number(argv[0]);
+let num = parseInt(argv[0]);
 if (isNaN(num)) console.log('Not a number');
 else console.log(`My number: ${num}`);
