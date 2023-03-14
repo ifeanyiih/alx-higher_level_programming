7adaeee52ee369a545660a3ba24568fc15c333d6
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-03-14 01:09:49 +0000

    add file

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+let argv = process.argv.slice(2);
+argv = argv.map(n => parseInt(n));
+argv.sort();
+if (argv.length === 0 || argv.length === 1) console.log(0);
+else console.log(argv[argv.length - 2]);
