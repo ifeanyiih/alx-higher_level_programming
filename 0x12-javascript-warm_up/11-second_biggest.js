147a01223569537e96f9677b074e3798f7854bc8
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-03-14 01:15:23 +0000

    modified

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 
 let argv = process.argv.slice(2);
 argv = argv.map(n => parseInt(n));
-argv.sort((a,b) => a - b);
+argv.sort((a, b) => a - b);
 if (argv.length === 0 || argv.length === 1) console.log(0);
 else console.log(argv[argv.length - 2]);
