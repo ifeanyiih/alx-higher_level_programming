649d2892d5e70b29aa5b496fc20e52c1b6173b7f
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-03-15 01:44:31 +0000

    modified

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,25 +4,27 @@ const fs = require('fs');
 
 const argv = process.argv.slice(2);
 
-fs.open(argv[2], 'a', (err, wfd) => {
-  if (err) console.log(err);
-  fs.open(argv[0], 'r', (err, fd) => {
+if (argv.length === 3) {
+  fs.open(argv[2], 'a', (err, wfd) => {
     if (err) console.log(err);
-    fs.read(fd, (err, bytesRead, buffer) => {
+    fs.open(argv[0], 'r', (err, fd) => {
       if (err) console.log(err);
-      fs.write(wfd, buffer, (err, written, string) => {
+      fs.read(fd, (err, bytesRead, buffer) => {
         if (err) console.log(err);
+        fs.write(wfd, buffer, (err, written, string) => {
+          if (err) console.log(err);
+        });
       });
     });
-  });
 
-  fs.open(argv[1], 'r', (err, fd) => {
-    if (err) console.log(err);
-    fs.read(fd, (err, bytesRead, buffer) => {
+    fs.open(argv[1], 'r', (err, fd) => {
       if (err) console.log(err);
-      fs.write(wfd, buffer, (err, written, string) => {
+      fs.read(fd, (err, bytesRead, buffer) => {
         if (err) console.log(err);
+        fs.write(wfd, buffer, (err, written, string) => {
+          if (err) console.log(err);
+        });
       });
     });
   });
-});
+}
