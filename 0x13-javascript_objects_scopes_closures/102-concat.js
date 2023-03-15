b5bea51ea1d0cab935132c71114d0ac99c19b224
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-03-15 13:03:14 +0000

    modified

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,23 +5,41 @@ const fs = require('fs');
 const argv = process.argv.slice(2);
 
 if (argv.length === 3) {
-  fs.open(argv[2], 'a', (err, wfd) => {
+  fs.open(argv[2], 'w', (err, wfd) => {
     if (err) console.log(err);
-    fs.open(argv[0], 'r', (err, fd) => {
+
+    fs.stat(argv[0], (err, stats) => {
       if (err) console.log(err);
-      fs.read(fd, (err, bytesRead, buffer) => {
+
+      fs.open(argv[0], 'r', (err, fd) => {
         if (err) console.log(err);
-        fs.write(wfd, buffer, (err, written, string) => {
+
+        const buffer = Buffer.alloc(stats.size);
+        fs.read(fd, buffer, 0, stats.size, -1, (err, bytesRead, buffer) => {
           if (err) console.log(err);
+
+          fs.write(wfd, buffer.toString('utf-8'), (err, written, string) => {
+            if (err) console.log(err);
+          });
         });
       });
+      fs.close(wfd, (err) => {
+        if (err) console.log(err);
+      });
     });
+  });
+
+  fs.stat(argv[1], (err, stats) => {
+    if (err) console.log(err);
 
     fs.open(argv[1], 'r', (err, fd) => {
       if (err) console.log(err);
-      fs.read(fd, (err, bytesRead, buffer) => {
+
+      const buffer = Buffer.alloc(stats.size);
+      fs.read(fd, buffer, 0, stats.size, -1, (err, bytesRead, buffer) => {
         if (err) console.log(err);
-        fs.write(wfd, buffer, (err, written, string) => {
+
+        fs.appendFile(argv[2], buffer.toString('utf-8'), (err) => {
           if (err) console.log(err);
         });
       });
