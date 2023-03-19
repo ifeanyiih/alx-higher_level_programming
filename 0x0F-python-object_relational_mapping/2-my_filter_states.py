d2a646b3c227739c1a8df09384ee36fb793e1a98
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-19 10:23:01 +0000

    Add
    Provides solution to task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+This module contains a script that lists all states from the database
+hbtn_0e_0_usa based on a users input
+"""
+
+import MySQLdb
+import sys
+
+
+def listStates(uname, passwd, dbname, uinput):
+    """Creates a connection to a database and lists the states
+    from database hbtn_0e_0_usa
+
+    Args:
+        uname (str): username
+        passwd (str): password
+        dbname (str): database to access
+        uinput (str): user input
+    Returns:
+        None
+    """
+    db = MySQLdb.connect(host="localhost", user=uname,
+                         passwd=passwd, db=dbname, port=3306)
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states WHERE"
+                f" name LIKE '{uinput}%' ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    uname, passwd, dbname, uinput = sys.argv[1:]
+    listStates(uname, passwd, dbname, uinput)
