defcdc440a6102b88df1fc0b8aa561a32cb61fc7
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-26 16:41:30 +0000

    modified

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 """
 This Module contains a python file that contains the class definition of a
 State and an instance Base = declarative_base().
 This module would prove my understanding of a creating Objects
 Maps using sqlalchemy.
 """
-
 
 from sqlalchemy.orm import declarative_state
 from sqlalchemy import Column, Integer, String
