fcfca96aa96616d430bc4004deaacc524e637d90
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-26 21:20:58 +0000

    modified

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,26 +12,20 @@ WARNING: all classes who inherit from Base must be imported
 before calling Base.metadata.create_all(engine)
 """
 
-from sqlalchemy.orm import declarative_state
-from sqlalchemy import Column, Integer, String
 
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Declares the state table
-    State Class that inherits from base linking to the states table
+    """State Class that inherits from base linking to the states table
     class attribute id that represents a column of an auto-generated,
     unique integer, can't be null and is a primary key
     class attribute name that represents a column of a string
-    with maximum 128 characters and can't be null
-
-    Attributes:
-        id (int): the class attribute
-        name (str): class attribute
-    """
-    __tablename___ = "states"
-
-    id = Column(Integer, primary_key=True)
+    with maximum 128 characters and can't be null"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
