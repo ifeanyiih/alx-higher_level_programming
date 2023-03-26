fec4588bc55512964e683f13ad1dd660ef3cb4f8
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-26 22:55:24 +0000

    add file

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module documentation
+========================
+
+Write a script that lists all State objects from the database hbtn_0e_6_usa
+
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state - from model_state import
+Base, State
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+The results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+
+
+if __name__ == '__main__':
+    user, passwd, db = sys.argv[1:]
+    engine = create_engine(f"mysql+mysqldb"
+                           f"://{user}:{quote_plus(passwd)}"
+                           f"@localhost:3306/{db}", echo=False)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).where(State.name.like('%a%')).delete()
+    session.commit()
