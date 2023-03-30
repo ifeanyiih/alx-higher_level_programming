f28d56dc9043f5f93127bfa1a09630d892a1e1d8
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-03-30 22:33:21 +0000

    Modified

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -I --no-progress-meter $1 | grep "Content-Length:" | cut -d " " -f 2 | grep -oE [0-9]+
+curl -I --no-progress-meter $1 | grep -oE "Content-Length: [0-9]+" | cut -d " " -f 2 | cat
