7d07a12047ef9befea21afb2da5d2473d890ca73
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-31 22:56:44 +0000

    Adds

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+
+The email must be sent in the email variable
+You must use the packages urllib and sys
+You are not allowed to import packages other than urllib and sys
+You don’t need to check arguments passed to the script (number or type)
+You must use the with statement
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url, email = sys.argv[1:]
+    payload = {}
+    payload["email"] = email
+    res = requests.post(url, data=payload)
+    print(res.text)
