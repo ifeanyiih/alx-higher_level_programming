0526c68e5fce5b3dfcba3ce124c0cef9e9f2c479
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-02 20:56:06 +0000

    Modified

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""
-Technical interview preparation:
-
+"""Technical interview preparation:
 You are not allowed to google anything
 Whiteboard first
 Write a function that finds a peak in a list of unsorted integers.
-
 Prototype: def find_peak(list_of_integers):
 You are not allowed to import any module
 Your algorithm must have the lowest complexity (hint: you don’t need
