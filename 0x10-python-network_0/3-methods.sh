211df591474dd496b9266bb80204229ea8105b2c
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-02 17:28:07 +0000

    MOdified

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -i -X OPTIONS $1 | grep "Allow: *"| cut -d " " -f1 --complement
+curl -si -X OPTIONS $1 | grep "Allow: *"| cut -d " " -f1 --complement
