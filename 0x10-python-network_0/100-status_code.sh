0862439a7d531e662e3107ed6e91644367a5c8ab
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-02 20:08:56 +0000

    Modified

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI "$1" | grep "HTTP*"| cut -d " " -f 2| cat
+curl -o /dev/null -sw "%{http_code}" "$1"
