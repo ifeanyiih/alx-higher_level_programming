e18671738886c4be910545ca8033af74d622fffb
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-02 17:22:33 +0000

    Modified

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-if [[ $(curl -o text -w "%{http_code}" "$1") == "200" ]];then cat text; rm text; fi; rm text
+if [[ $(curl -L "$1") == "200" ]];then cat text; rm text; fi; rm text
