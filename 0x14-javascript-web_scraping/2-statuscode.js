a7edfaad2b54b791b0c76339ce16f61dd6cd8f63
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-25 14:02:01 +0000

    modified

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,9 @@
 const request = require('request');
 
 const url = process.argv[2];
-request(url, function (error, response, body) {
-  if (error) console.log(error);
-  console.log('code: ', response.statusCode);
-});
+
+request
+  .get(url)
+  .on('response', function (response) {
+    console.log('code: ', response.statusCode); // 200
+  });
