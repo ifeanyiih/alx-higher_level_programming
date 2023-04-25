8f30ec97d70530fab1e04ee31163115f70facec8
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-25 15:05:21 +0000

    Adds

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+const taskobj = {};
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  const todos = JSON.parse(body);
+  for (const todo of todos) {
+    if (!Object.keys(taskobj).includes(String(todo.userId))) {
+      let count = 0;
+      if (todo.completed) count += 1;
+      taskobj[todo.userId] = count;
+    } else {
+      if (todo.completed) taskobj[todo.userId]++;
+    }
+  }
+  console.log(taskobj);
+});
